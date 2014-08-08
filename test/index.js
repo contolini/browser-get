b937b38238cdba43c12e8e34596ac52f4f7fe729
Chris Contolini <christopher.contolini@cfpb.gov>
2014-08-07 22:31:51 -0400

    Keep sinon dep local

--- test/index.js
+++ test/index.js
@@ -8,15 +8,11 @@ server.autoRespond = true;
 
 var promise = get('/foo');
 
-server.respond();
-
-// When request completes.
 promise.then(function( resp ){
   console.log(resp);
   document.getElementById('console').value = resp;
 });
 
-// If request fails.
 promise.catch(function( resp ){
   console.log( resp );
 });
